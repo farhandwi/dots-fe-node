cfb850585ceb9349d14677a5009044030ca4b00f
farhandwi <farhandwi234@gmail.com>
2025-02-17 10:54:59 +0700

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/dots',
+  assetPrefix: '/dots',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
